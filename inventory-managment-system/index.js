9b1a4c9fab9bc15a917698a49983066f44bfcba1
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-05-16 13:47:49 +0500

    feat: add inventory main index file

---
+++ inventory-managment-system/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { PORT } from "./config/env.js";
+import { connectDB } from "./config/db.connection.js";
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});

f89e7d4d0bec3945b645ccb37191c508819726ca
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-05-26 12:24:33 +0500

    add supplier model

---
+++ inventory-managment-system/models/supplier.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const supplierSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    companyName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Supplier = mongoose.model("Supplier", supplierSchema);

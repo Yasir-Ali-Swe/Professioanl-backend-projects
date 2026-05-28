f02789dc5c7c56baefc148fbae22afd7fd8f9e42
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-05-28 16:12:18 +0500

    update environment configuration

--- inventory-managment-system/config/env.js
+++ inventory-managment-system/config/env.js
@@ -1,4 +1,19 @@
 import "dotenv/config";
 
-export const PORT = process.env.PORT;
-export const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+const JWT_SECRET = process.env.JWT_SECRET;
+const EMAIL_USER = process.env.EMAIL_USER;
+const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+const CLIENT_URL = process.env.CLIENT_URL;
+const NODE_ENV = process.env.NODE_ENV;
+
+export {
+  PORT,
+  MONGODB_URI,
+  JWT_SECRET,
+  EMAIL_USER,
+  EMAIL_PASSWORD,
+  CLIENT_URL,
+  NODE_ENV,
+};

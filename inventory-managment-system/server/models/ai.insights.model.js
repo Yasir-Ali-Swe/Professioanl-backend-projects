eeb993e3ddcf5238a8a42def213894cf538198d7
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-04 13:47:37 +0500

    add AI insights model

---
+++ inventory-managment-system/server/models/ai.insights.model.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const aiInsightsSchema = new mongoose.Schema({
+  organizationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Organization',
+    required: true
+  },
+  period: {
+    type: String,
+    enum: ["weekly", "monthly"]
+  },
+  summaryText: {
+    type: String
+  },
+  keyMetrics: {
+    topSellingProductId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      default: null
+    },
+    decliningProductId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      default: null
+    },
+    totalRevenue: {
+      type: Number,
+      required: true
+    },
+    totalOrders: {
+      type: Number,
+      required: true
+    }
+  },
+}, {
+  timestamps: true
+});
+
+const AiInsights = mongoose.model('AiInsights', aiInsightsSchema);
+
+export default AiInsights;

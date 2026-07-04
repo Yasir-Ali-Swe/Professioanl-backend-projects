aef3695093d043744e5c1c25348e682d59006cb9
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-04 14:08:32 +0500

    update purchase order model

--- inventory-managment-system/server/models/purchase.order.model.js
+++ inventory-managment-system/server/models/purchase.order.model.js
@@ -10,7 +10,7 @@ const purchaseOrderSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  supplier: {
+  supplierId : {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Supplier',
     required: true

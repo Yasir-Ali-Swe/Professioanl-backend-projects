ce8b97a93ffea2b74656b77fb0b47ae898b6eb3b
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-05 13:12:44 +0500

    update auth routes

--- inventory-managment-system/server/routes/auth.routes.js
+++ inventory-managment-system/server/routes/auth.routes.js
@@ -1,10 +1,23 @@
-import { registerOrganization } from "../controllers/auth.controller.js";
+import {
+  registerOrganization,
+  loginUser,
+  logoutUser,
+  getLoginUser,
+  forgetPassword,
+  resetPassword,
+  refreshAuth,
+} from "../controllers/auth.controller.js";
 import express from "express";
 import { authMiddleware } from "../middleware/auht.middleware.js";
 
 const router = express.Router();
 
 router.post("/register-organization", registerOrganization);
-
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.get("/get-login-user", authMiddleware, getLoginUser);
+router.post("/forget-password", forgetPassword);
+router.post("/reset-password/:token", resetPassword);
+router.post("/refresh-auth", refreshAuth);
 
 export default router;

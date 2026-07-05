77eeddbbfd010ea7fa4850dbdf1718b87452e826
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-05 13:12:38 +0500

    update user model

--- inventory-managment-system/server/models/user.model.js
+++ inventory-managment-system/server/models/user.model.js
@@ -44,6 +44,10 @@ const userSchema = new mongoose.Schema(
       ref: "User",
       default: null,
     },
+    tokenVersion: {
+      type: Number,
+      default: 0,
+    },
   },
   { timestamps: true },
 );

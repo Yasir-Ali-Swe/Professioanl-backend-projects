8723d4e01bb086562f100a4d57d842f407513d35
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-06 12:30:49 +0500

    update server index

--- inventory-managment-system/server/index.js
+++ inventory-managment-system/server/index.js
@@ -8,6 +8,7 @@ import superAdminRoutes from "./routes/superAdmin.routes.js";
 import organizationAdminRoutes from "./routes/organizationAdmin.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 import supplierRoutes from "./routes/supplier.route.js";
+import productRoutes from "./routes/product.routes.js";
 
 
 const app = express();
@@ -26,3 +27,4 @@ app.use("/api/v1/super-admin", superAdminRoutes);
 app.use("/api/v1/organization-admin", organizationAdminRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/supplier", supplierRoutes);
+app.use("/api/v1/product", productRoutes);

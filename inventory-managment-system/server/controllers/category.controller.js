0e0d8040d16e4749d0db8240bd40f1e1e14ad507
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-06 20:47:42 +0500

    update category controller

--- inventory-managment-system/server/controllers/category.controller.js
+++ inventory-managment-system/server/controllers/category.controller.js
@@ -1,4 +1,4 @@
-import categoryModel from "../models/categorie.model.js";
+import categoryModel from "../models/category.model.js";
 import slugify from "slugify";
 import productModel from "../models/product.model.js";
 

5c3b0cac1b75045738cab29d416c95b2e9af426e
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-06 20:30:22 +0500

    add purchaseOrder routes

---
+++ inventory-managment-system/server/routes/purchaseOrder.routes.js
@@ -0,0 +1,57 @@
+import express from "express";
+import {
+  createPurchaseOrder,
+  getAllPurchaseOrders,
+  getPurchaseOrderById,
+  approvePurchaseOrder,
+  rejectPurchaseOrder,
+  fulfillPurchaseOrder,
+} from "../controllers/purchase.order.controller.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
+import { authorize } from "../middleware/authorize.user.middleware.js";
+
+const router = express.Router();
+
+router.post(
+  "/create-purchase-order",
+  authMiddleware,
+  authorize("admin", "manager"),
+  createPurchaseOrder,
+);
+
+router.get(
+  "/get-all-purchase-orders",
+  authMiddleware,
+  authorize("admin", "manager"),
+  getAllPurchaseOrders,
+);
+
+router.get(
+  "/get-purchase-order-by-id/:id",
+  authMiddleware,
+  authorize("admin", "manager"),
+  getPurchaseOrderById,
+);
+
+router.patch(
+  "/approve-purchase-order/:id",
+  authMiddleware,
+  authorize("admin", "manager"),
+  approvePurchaseOrder,
+);
+
+router.patch(
+  "/reject-purchase-order/:id",
+  authMiddleware,
+  authorize("admin", "manager"),
+  rejectPurchaseOrder,
+);
+
+router.patch(
+  "/fulfill-purchase-order/:id",
+  authMiddleware,
+  authorize("admin", "manager"),
+  fulfillPurchaseOrder,
+);
+
+export default router;

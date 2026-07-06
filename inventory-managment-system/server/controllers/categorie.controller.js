d2c3f5cedf08899ee96aa8bcc4156298e7ae67d7
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-06 11:12:40 +0500

    update categories controller

--- inventory-managment-system/server/controllers/categorie.controller.js
+++ inventory-managment-system/server/controllers/categorie.controller.js
@@ -18,18 +18,54 @@ export const createCategory = async (req, res) => {
       categorySlug,
       createdBy,
     });
-    res
-      .status(201)
-      .json({
-        success: true,
-        message: "Category created successfully",
-        data: category,
-      });
+    res.status(201).json({
+      success: true,
+      message: "Category created successfully",
+      data: category,
+    });
   } catch (error) {
     console.error("Error creating category:", error.message);
-    res
-      .status(error.status || 500)
-      .json({ message: error.message || "Internal server error" });
+    res.status(error.status || 500).json({
+      success: false,
+      message: error.message || "Internal server error",
+    });
+  }
+};
+
+export const updateCategory = async (req, res) => {
+  try {
+    const organizationId = req.organizationId;
+    const createdBy = req.user._id;
+    if (!organizationId) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Organization ID is required" });
+    }
+    const categoryId = req.params.id;
+    const { name } = req.body;
+    if (!name) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Category name is required" });
+    }
+    const categorySlug = slugify(name, { lower: true, strict: true });
+
+    const updatedCategory = await categoryModel.findOneAndUpdate(
+      { _id: categoryId, organizationId },
+      { name, categorySlug },
+      { new: true },
+    );
+    res.status(200).json({
+      success: true,
+      message: "Category updated successfully",
+      data: updatedCategory,
+    });
+  } catch (error) {
+    console.error("Error updating category:", error.message);
+    res.status(error.status || 500).json({
+      success: false,
+      message: error.message || "Internal server error",
+    });
   }
 };
 
@@ -37,7 +73,9 @@ export const getAllCategories = async (req, res) => {
   try {
     const organizationId = req.organizationId;
     if (!organizationId) {
-      return res.status(400).json({ message: "Organization ID is required" });
+      return res
+        .status(400)
+        .json({ success: false, message: "Organization ID is required" });
     }
     const categories = await categoryModel
       .find({ organizationId })
@@ -46,9 +84,10 @@ export const getAllCategories = async (req, res) => {
     res.status(200).json({ success: true, data: categories });
   } catch (error) {
     console.error("Error fetching categories:", error.message);
-    res
-      .status(error.status || 500)
-      .json({ message: error.message || "Internal server error" });
+    res.status(error.status || 500).json({
+      success: false,
+      message: error.message || "Internal server error",
+    });
   }
 };
 
@@ -57,20 +96,59 @@ export const getCategoryById = async (req, res) => {
     const organizationId = req.organizationId;
     const categoryId = req.params.id;
     if (!organizationId) {
-      return res.status(400).json({ message: "Organization ID is required" });
+      return res
+        .status(400)
+        .json({ success: false, message: "Organization ID is required" });
     }
     const category = await categoryModel
       .findById(categoryId)
       .select("-__v -updatedAt")
       .lean();
     if (!category) {
-      return res.status(404).json({ message: "Category not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "Category not found" });
     }
     res.status(200).json({ success: true, data: category });
   } catch (error) {
     console.error("Error fetching category by ID:", error.message);
+    res.status(error.status || 500).json({
+      success: false,
+      message: error.message || "Internal server error",
+    });
+  }
+};
+
+export const deleteCategory = async (req, res) => {
+  try {
+    const organizationId = req.organizationId;
+    const categoryId = req.params.id;
+
+    if (!organizationId) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Organization ID is required" });
+    }
+
+    const deletedCategory = await categoryModel.findOneAndDelete({
+      _id: categoryId,
+      organizationId,
+    });
+
+    if (!deletedCategory) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Category not found" });
+    }
+
     res
-      .status(error.status || 500)
-      .json({ message: error.message || "Internal server error" });
+      .status(200)
+      .json({ success: true, message: "Category deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting category:", error.message);
+    res.status(error.status || 500).json({
+      success: false,
+      message: error.message || "Internal server error",
+    });
   }
 };

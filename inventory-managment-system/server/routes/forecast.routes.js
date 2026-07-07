4c44e26e747a6934abf285efd48a58d5c2887b17
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-07 12:01:34 +0500

    add forecast routes

---
+++ inventory-managment-system/server/routes/forecast.routes.js
@@ -0,0 +1,49 @@
+import express from "express";
+import {
+  getForecastForProduct,
+  getAllForecasts,
+  getReorderSuggestions,
+  approveReorderSuggestion,
+  dismissReorderSuggestion,
+} from "../controllers/forecast.controller.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
+import { authorize } from "../middleware/authorize.user.middleware.js";
+
+const router = express.Router();
+
+router.get(
+  "/forecast/:id",
+  authMiddleware,
+  authorize("admin", "manager"),
+  getForecastForProduct,
+);
+
+router.get(
+  "/forecasts",
+  authMiddleware,
+  authorize("admin", "manager"),
+  getAllForecasts,
+);
+
+router.get(
+  "/reorder-suggestions",
+  authMiddleware,
+  authorize("admin", "manager"),
+  getReorderSuggestions,
+);
+
+router.patch(
+  "/reorder-suggestions/:id/approve",
+  authMiddleware,
+  authorize("admin", "manager"),
+  approveReorderSuggestion,
+);
+
+router.patch(
+  "/reorder-suggestions/:id/dismiss",
+  authMiddleware,
+  authorize("admin", "manager"),
+  dismissReorderSuggestion,
+);
+
+export default router;

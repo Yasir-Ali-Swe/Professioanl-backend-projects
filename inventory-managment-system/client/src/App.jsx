e867de0cfe45a01327823967987fa38bb22f108e
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-07 22:14:13 +0500

    add App component

---
+++ inventory-managment-system/client/src/App.jsx
@@ -0,0 +1,10 @@
+import {Button} from "@/components/ui/button"
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default App

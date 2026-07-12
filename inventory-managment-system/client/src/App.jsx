61b73af1d296d6e708aced1237a899fde33f70be
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-12 14:26:39 +0500

    update App component

--- inventory-managment-system/client/src/App.jsx
+++ inventory-managment-system/client/src/App.jsx
@@ -1,8 +1,11 @@
-import {Button} from "@/components/ui/button"
+import { Routes, Route } from "react-router-dom"
+import NotFoundPage from "@/pages/NotFoundPage";
+import TableSkeleton from "./components/skeletons/TableSkeleton";
+import DashboardSkeleton from "./components/skeletons/DashboardSkeleton";
 const App = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <Button>Click Me</Button>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-background">
+      <TableSkeleton />
     </div>
   )
 }

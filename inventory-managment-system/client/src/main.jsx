725a1a4cf7386fa922be6ef1d3869d0044d546d4
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-12 14:26:49 +0500

    update main entry point

--- inventory-managment-system/client/src/main.jsx
+++ inventory-managment-system/client/src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { TooltipProvider } from "@/components/ui/tooltip"
+import { BrowserRouter } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
   <TooltipProvider>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </TooltipProvider>
 )

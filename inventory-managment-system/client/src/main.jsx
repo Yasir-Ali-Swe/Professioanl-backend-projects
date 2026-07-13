c623eebedf3054e6b3b2e72c7483a1c27162113f
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-13 10:28:00 +0500

    update main entry point

--- inventory-managment-system/client/src/main.jsx
+++ inventory-managment-system/client/src/main.jsx
@@ -3,10 +3,12 @@ import './index.css'
 import App from './App.jsx'
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { BrowserRouter } from "react-router-dom"
+import { Toaster } from "@/components/ui/sonner"
 
 createRoot(document.getElementById('root')).render(
   <TooltipProvider>
     <BrowserRouter>
+      <Toaster richColors position="top-right" />
       <App />
     </BrowserRouter>
   </TooltipProvider>

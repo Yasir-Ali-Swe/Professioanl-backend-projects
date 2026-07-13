3441296c700d10deb694ccc7995858e974f174b4
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-13 13:52:48 +0500

    update main entry point

--- inventory-managment-system/client/src/main.jsx
+++ inventory-managment-system/client/src/main.jsx
@@ -4,12 +4,20 @@ import App from './App.jsx'
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { BrowserRouter } from "react-router-dom"
 import { Toaster } from "@/components/ui/sonner"
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from "./store/index.js";
+import { ThemeProvider } from "@/components/ThemeProvider.jsx";
 
 createRoot(document.getElementById('root')).render(
   <TooltipProvider>
     <BrowserRouter>
       <Toaster richColors position="top-right" />
-      <App />
+      <Provider store={store}>
+        <ThemeProvider defaultTheme="light" storageKey="stockpilot-theme">
+          <App />
+        </ThemeProvider>
+      </Provider>
     </BrowserRouter>
   </TooltipProvider>
 )

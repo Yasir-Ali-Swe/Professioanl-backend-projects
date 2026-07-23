a20496808a673c89d94449b9c060487178886832
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-23 16:28:24 +0500

    update store index

--- inventory-managment-system/client/src/store/index.js
+++ inventory-managment-system/client/src/store/index.js
@@ -2,13 +2,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import uiReducer from "./slices/uiSlice";
-import organizationReducer from "./slices/organizationSlice";
-
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     ui: uiReducer,
-    organization: organizationReducer,
     // Add more slices as needed
   },
   middleware: (getDefaultMiddleware) =>

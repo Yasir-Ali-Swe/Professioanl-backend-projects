ef418aa26f4e58c574ee32d00d6d5c28a277b333
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-23 16:28:28 +0500

    update rootReducer

--- inventory-managment-system/client/src/store/rootReducer.js
+++ inventory-managment-system/client/src/store/rootReducer.js
@@ -2,13 +2,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import uiReducer from "./slices/uiSlice";
-import organizationReducer from "./slices/organizationSlice";
-import productReducer from "./slices/productSlice";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
   ui: uiReducer,
-  organization: organizationReducer,
-  product: productReducer,
-  // Add more reducers as needed
 });

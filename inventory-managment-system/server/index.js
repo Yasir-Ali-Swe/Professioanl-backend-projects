f92333e4a8bc46801bb902f29cea5537274071ff
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-23 16:29:28 +0500

    update server index

--- inventory-managment-system/server/index.js
+++ inventory-managment-system/server/index.js
@@ -28,7 +28,10 @@ import morgan from "morgan";
 const app = express();
 
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(cookieParser());
 
 app.use("/api/v1/billing", billingRoutes);

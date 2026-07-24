d9791da18a8e752a3e9befad117726d7c4edafe1
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-24 13:47:46 +0500

    update main entry point

--- inventory-managment-system/client/src/main.jsx
+++ inventory-managment-system/client/src/main.jsx
@@ -1,32 +1,3 @@
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import { TooltipProvider } from "@/components/ui/tooltip"
-// import { BrowserRouter } from "react-router-dom"
-// import { Toaster } from "@/components/ui/sonner"
-// import { Provider } from 'react-redux';
-// import { store } from "./store/index.js";
-// import { ThemeProvider } from "@/components/ThemeProvider.jsx";
-// import { QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-// import { queryClient } from "@/lib/queryClient.js";
-
-// createRoot(document.getElementById('root')).render(
-//   <TooltipProvider>
-//     <BrowserRouter>
-//       <QueryClientProvider client={queryClient}>
-//         <Toaster richColors position="bottom-right" />
-//         <Provider store={store}>
-//           <ThemeProvider defaultTheme="light" storageKey="stockpilot-theme">
-//             <App />
-//           </ThemeProvider>
-//         </Provider>
-//         <ReactQueryDevtools initialIsOpen={false} />
-//       </QueryClientProvider>
-//     </BrowserRouter>
-//   </TooltipProvider>
-// )
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'

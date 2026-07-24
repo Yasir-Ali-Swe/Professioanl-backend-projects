cca4eecabe5f0e64ea4140620c3712fdaf7d2121
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-24 13:48:59 +0500

    update StockList page

--- inventory-managment-system/client/src/pages/stock/StockList.jsx
+++ inventory-managment-system/client/src/pages/stock/StockList.jsx
@@ -246,7 +246,7 @@ const StockList = () => {
             </div>
 
             {/* Table */}
-            <div className="border overflow-hidden">
+            <div className="border rounded-xl overflow-hidden bg-card">
                 <div className="overflow-x-auto">
                     <Table>
                         <TableHeader>

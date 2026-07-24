8afccd417c9c33be9b3939f9d6964eec73e00f56
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-07-24 13:47:43 +0500

    update AuthLayout component

--- inventory-managment-system/client/src/layouts/AuthLayout.jsx
+++ inventory-managment-system/client/src/layouts/AuthLayout.jsx
@@ -1,11 +1,19 @@
-import { Outlet } from "react-router-dom";
 
+// export const AuthLayout = () => {
+//     return (
+//         <div className="min-h-screen flex items-center justify-center">
+//             <div className="w-full">
+//                 <Outlet />
+//             </div>
+//         </div>
+//     );
+// };
+
+import { Outlet } from "react-router-dom";
 export const AuthLayout = () => {
     return (
-        <div className="min-h-screen flex items-center justify-center">
-            <div className="w-full">
-                <Outlet />
-            </div>
+        <div className="min-h-screen">
+            <Outlet />
         </div>
     );
 };
